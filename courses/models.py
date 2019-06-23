cf90495a1ea391555ed5d410c2e7b6644674fb4d
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-06-23 19:56:23 +0300

    DEBUG: remove errornous model configurations that were interfearing with the migration process

--- courses/models.py
+++ courses/models.py
@@ -3,9 +3,9 @@ from django.db import models
 
 # Create your models here.
 class Course(models.Model):
-    title = models.CharField(max_length=255),
-    description = models.TextField(),
-    created_at = models.DateTimeField(auto_now_add=True),
+    title = models.CharField(max_length=255)
+    description = models.TextField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
         db_table = "courses"

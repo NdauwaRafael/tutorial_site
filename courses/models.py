fef75dd8ab2a2f049f6e4dc3c74f50eb21c1f044
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-06-23 20:29:46 +0300

    return title as string from courses model

--- courses/models.py
+++ courses/models.py
@@ -8,5 +8,8 @@ class Course(models.Model):
     difficulty = models.CharField(max_length=30, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.title
+
     class Meta:
         db_table = "courses"

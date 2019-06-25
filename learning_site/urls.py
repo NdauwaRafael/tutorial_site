5d8bfd5bb8d0ad8befc3efcb320ee939ab7746ee
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-06-25 21:05:31 +0300

    add static files url config in main app urls

--- learning_site/urls.py
+++ learning_site/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
-
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index ),
+    path('', views.index),
     path('courses/', include('courses.urls')),
 ]
+
+urlpatterns += staticfiles_urlpatterns()

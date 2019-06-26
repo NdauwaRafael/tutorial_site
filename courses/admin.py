bf251624093e63fab69af52d2ba75ce89d545fb8
Raphael Ndauwa <raphaelndauwa@gmail.com>
2019-06-26 20:35:40 +0300

    add course adn courseAdmin to control how one add steps for a specific course

--- courses/admin.py
+++ courses/admin.py
@@ -2,6 +2,16 @@ from django.contrib import admin
 from .models import Course, Step
 
 
+# Add a form
+class StepInLine(admin.StackedInline):
+    model = Step
+
+
+# Course Admin
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [StepInLine, ]
+
+
 # Register your models here.
-admin.site.register(Course)
+admin.site.register(Course, CourseAdmin)
 admin.site.register(Step)
